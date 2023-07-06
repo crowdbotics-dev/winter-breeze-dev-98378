9e7c28df1ae15f52dc68dc5ec256838fca84ee7b
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 13:17:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-breeze-dev-98378",
+  url: "https://winter-breeze-dev-98378.botics.co",
+  api: "https://winter-breeze-dev-98378.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
